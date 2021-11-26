21da09024d2b6798d96b2a9b0c21c78b12dd3968
Jonatan Uribe <jonug84@gmail.com>
2021-11-25 21:47:30 -0500

    commit funcionando

---
+++ src/main/java/co/edu/udem/devops/taller1/Taller14F/Taller14FApplication.java
@@ -0,0 +1,14 @@
+package co.edu.udem.devops.taller1.Taller14F;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller14FApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		SpringApplication.run(Taller14FApplication.class, args);
+	}
+
+}
